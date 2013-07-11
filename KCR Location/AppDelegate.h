377904d3154057cbb8f193d4a8c85dc351eb427e
Raymond Lee <leeraymond@Raymonds-MacBook-Pro-3.local>
2013-07-12 00:08:51 +0800

    Initial Commit

---
+++ KCR Location/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  KCR Location
+//
+//  Created by Raymond Lee on 12/7/13.
+//  Copyright (c) 2013 RayCom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ KCR Location/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KCR Location
+//
+//  Created by Raymond Lee on 12/7/13.
+//  Copyright (c) 2013 RayCom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
